e82d286e9dd26787367b2e5d98a187f2d09ffca9
zeeshansalim1234 <59512700+zeeshansalim1234@users.noreply.github.com>
2022-01-13 01:37:08 -0700

    Update arr-Q1.py

--- arr-Q1.py
+++ arr-Q1.py
@@ -1,8 +1,9 @@
-counter=1
-        for i in range(1,len(nums)):
-            if(nums[i]!=nums[i-1]):
-                nums[counter]=nums[i]
-                counter+=1
-                
-        del nums[counter+1:]
-        return counter
+def removeDuplicates(self, nums):
+        counter=1
+                for i in range(1,len(nums)):
+                    if(nums[i]!=nums[i-1]):
+                        nums[counter]=nums[i]
+                        counter+=1
+
+                del nums[counter+1:]
+                return counter
